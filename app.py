c6397cfaaac41b5f42f3360862c31f0672ac26fe
rubbymf10 <rubbyskripsi.id@gmail.com>
2025-06-27 10:21:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -195,7 +195,7 @@ if halaman == "Histori":
         st.info("Belum ada pencarian.")
 
     st.markdown("---")
-    st.header("🎧 Rekomendasi Genre Terakhir")
+    st.header("🎧 Hasil Rekomendasi Terakhir")
     if not st.session_state.recommendation_table.empty:
         df_show = st.session_state.recommendation_table.sort_values(by='popularity', ascending=False)
         for _, row in df_show.iterrows():
